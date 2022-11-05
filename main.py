2302282c433f415970cf1a7a76330af19a633946
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-11-05 13:04:55 +0000

    EJERCICIO 1 TERMINADO

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-for i in range(5):
-  print('A number:', i)
+print("\n\n")
+
+class Alumno:
+  def __init__(self, nombre, nota):
+    self.nombre= nombre
+    self.nota= nota
+    print("El alumno se ha creado con éxito\n")
+
+  def datos(self):
+    print("Nombre: ",self.nombre)
+    print("Nota: ",self.nota)
+
+  def califiacion(self):
+    if self.nota>=5:
+      print(self.nombre, "ha aprobado")
+    else:
+      print(self.nombre, "ha suspendido")
+
+
+alumno1=Alumno("Jaime", 5)
+alumno1.datos()
+alumno1.califiacion()
+
+print("\n\n")
+
+alumno2=Alumno("Alejandro", 3)
+alumno2.datos()
+alumno2.califiacion()
+  
+  
