3d9cc37a42d529606a1f89a1efd8109512a488ff
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-11-05 15:54:18 +0000

    _str__ del ejercicio 2 terminado

---
+++ ejercicio2.py
@@ -0,0 +1,27 @@
+#EJERCICIO 1 
+
+print("\n\n")
+#VAMOS A CREAR UNA CLASE LLAMADA ALUMNO, CON SU CONSTRUCTOR Y SUS ATRIBUTOS NOMBRE Y NOTA. CUANDO SE HAYA CREADO LA CLASE INDICAREMOS QUE SE HA CREADO CON ÉXITO Y POSTERIORMENTE CREAREMOS EL MÉTODO CALIFIACIÓN PARA PODER SABER SI EL ALUMNO HA APROBADO O SUSPENDIDO. 
+
+class Alumno:
+  def __init__(self, nombre, nota):
+    self.nombre= nombre
+    self.nota= nota
+    print("El alumno se ha creado con éxito\n")
+  
+  def califiacion(self):
+    if self.nota>=5:
+      print(self.nombre, "ha aprobado")
+    else:
+      print(self.nombre, "ha suspendido")
+  
+  def __str__(self):
+    return "Nombre: {}\n Nota: {}".format(self.nombre, self.nota)
+
+
+
+alumno1=Alumno("Jaime", 5)
+print(alumno1)
+
+  
+  
--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+#EJERCICIO 2
+
 print("\n\n")
+#EN UNA PRIMERA PARTE EL CÓDIGO ES IGUAL QUE EN EL EJERCICIO 1. Sin embargo, para este ejercicio utilizaremos el metodo __str__; este metodo nos sirve para imprimir por pantalla la información de un objeto, si tenemos un objeto alumno1 creado y realizamos print(alumno1), Python ejecutará el contenido del método str (el método str lo que tiene que hacer es maquetar la información que desea en un string).
 
-class Producto:
-  def __init__(self, codigo, nombre, precio, tipo):
-    print("El producto se ha creado con éxito")
-    self.codigo= codigo
+
+class Alumno:
+  def __init__(self, nombre, nota):
     self.nombre= nombre
-    self.precio= precio
-    self.tipo = tipo
+    self.nota= nota
+    print("El alumno se ha creado con éxito\n")
+  
+  def calificacion(self):
+    if self.nota>=5:
+      print(self.nombre, "ha aprobado")
+    else:
+      print(self.nombre, "ha suspendido")
 
+  
   def __str__(self):
-        return """\
-CODIGO\t{}
-NOMBRE\t\t{}
-PRECIO\t\t{}
-TIPO\t{}""".format(self.codigo, self.nombre, self.precio, self.tipo)
+    return "{}, ha sacado un: {}".format(self.nombre, self.nota)
+
+alumno1=Alumno("Jaime", 5)
+print(alumno1)
+calificacion= alumno1.calificacion()
+
+
