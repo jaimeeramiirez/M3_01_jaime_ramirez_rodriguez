b7595f9bb46552560e0748e926386d6649528f9e
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-11-05 13:10:30 +0000

    EJERCICIO 1 MODIFICADO, HE ELEMINADO EL METODO DATOS YA QUE PODÍA METERSE DENTRO DEL __INIT__

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 print("\n\n")
+#VAMOS A CREAR UNA CLASE LLAMADA ALUMNO, CON SU CONSTRUCTOR Y SUS ATRIBUTOS NOMBRE Y NOTA. CUANDO SE HAYA CREADO LA CLASE INDICAREMOS QUE SE HA CREADO CON ÉXITO Y POSTERIORMENTE CREAREMOS EL MÉTODO CALIFIACIÓN PARA PODER SABER SI EL ALUMNO HA APROBADO O SUSPENDIDO. 
 
 class Alumno:
   def __init__(self, nombre, nota):
     self.nombre= nombre
     self.nota= nota
     print("El alumno se ha creado con éxito\n")
-
-  def datos(self):
-    print("Nombre: ",self.nombre)
-    print("Nota: ",self.nota)
-
+    print("Nombre: ", self.nombre)
+    print("Nota: ", self.nota)
+  
   def califiacion(self):
     if self.nota>=5:
       print(self.nombre, "ha aprobado")
@@ -17,14 +16,13 @@ class Alumno:
       print(self.nombre, "ha suspendido")
 
 
+
 alumno1=Alumno("Jaime", 5)
-alumno1.datos()
 alumno1.califiacion()
 
 print("\n\n")
 
 alumno2=Alumno("Alejandro", 3)
-alumno2.datos()
 alumno2.califiacion()
   
   
