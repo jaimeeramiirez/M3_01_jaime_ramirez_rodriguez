bfe3c04a526d3c8ff37a57ae34b2efa52a2c5b3f
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-11-05 16:09:28 +0000

    ejercicio 2 comentado

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,7 +1,8 @@
-#EJERCICIO 1 
+#EJERCICIO 2
 
 print("\n\n")
-#VAMOS A CREAR UNA CLASE LLAMADA ALUMNO, CON SU CONSTRUCTOR Y SUS ATRIBUTOS NOMBRE Y NOTA. CUANDO SE HAYA CREADO LA CLASE INDICAREMOS QUE SE HA CREADO CON ÉXITO Y POSTERIORMENTE CREAREMOS EL MÉTODO CALIFIACIÓN PARA PODER SABER SI EL ALUMNO HA APROBADO O SUSPENDIDO. 
+#EN UNA PRIMERA PARTE EL CÓDIGO ES IGUAL QUE EN EL EJERCICIO 1. Sin embargo, para este ejercicio utilizaremos el metodo __str__; este metodo nos sirve para imprimir por pantalla la información de un objeto, si tenemos un objeto alumno1 creado y realizamos print(alumno1), Python ejecutará el contenido del método str (el método str lo que tiene que hacer es maquetar la información que desea en un string).
+
 
 class Alumno:
   def __init__(self, nombre, nota):
@@ -9,19 +10,18 @@ class Alumno:
     self.nota= nota
     print("El alumno se ha creado con éxito\n")
   
-  def califiacion(self):
+  def calificacion(self):
     if self.nota>=5:
       print(self.nombre, "ha aprobado")
     else:
       print(self.nombre, "ha suspendido")
+
   
   def __str__(self):
-    return "Nombre: {}\n Nota: {}".format(self.nombre, self.nota)
-
-
+    return "{}, ha sacado un: {}".format(self.nombre, self.nota)
 
 alumno1=Alumno("Jaime", 5)
 print(alumno1)
+calificacion= alumno1.calificacion()
 
-  
-  
+
